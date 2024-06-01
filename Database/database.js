98020117c831429fe37f26c1a383cbca4687cd40
saileshghimire <saileshghimire1@gmail.com>
2024-06-01 09:49:19 +0545

    database framework creation

--- Database/database.js
+++ Database/database.js
@@ -1,3 +1,19 @@
-const mongoose = require('mongoose');
+import mongoose from 'mongoose';
 
 mongoose.connect('url');
+
+// Defining Schema
+const AdminSchema = new mongoose.Schema({
+    username: String,
+    password: String
+});
+
+const UserSchema = new mongoose.Schema({
+    username: String,
+    password:String,
+    firstname: String,
+    middlename: String,
+    lastname: String
+    
+    
+})
